9738b212914ce560f8d2fc0f26e3002f2aaab518
DHRUVIL18 <dhruvil18022001@gmail.com>
2024-06-23 02:54:02 -0400

    Restaurant

--- FoodDelivery/restaurant/models.py
+++ FoodDelivery/restaurant/models.py
@@ -7,16 +7,20 @@ class RestaurantProfile(Audit):
         db_table = "fd_restaurant_profile"
 
     restaurant_id = models.BigAutoField(primary_key=True)
-    # restaurant_name = models.CharField(max_length=255)
+   
+    restaurant_name = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
     email = models.EmailField(unique=True)
-    # address = models.CharField(max_length=255)
-    # restaurant_product_review_average = models.CharField(max_length=255)
+
 
 class RestaurantOTP(Audit):
     class Meta:
         db_table = "fd_restaurant_otp"
 
     restaurant_otp_id = models.BigAutoField(primary_key=True)
+    
     restaurant_id = models.ForeignKey(RestaurantProfile, on_delete=models.CASCADE)
+    
     otp = models.CharField(max_length=255)
--- FoodDelivery/restaurant/serializers.py
+++ FoodDelivery/restaurant/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import RestaurantOTP, RestaurantProfile, RestaurantOTP
 
 
-#from administrator.models import Holidays
+
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
