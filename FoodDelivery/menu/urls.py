9e6483864f9a9143cf685576bacbee4e11e825a5
prakruti58 <prakruti0502@gmail.com>
2024-06-25 04:01:59 -0400

    created api's to create,delete.update menucategory and menu item and given path in urls

--- FoodDelivery/menu/urls.py
+++ FoodDelivery/menu/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import MenuCategoryCreate
+from .views import MenuCategoryCreate, MenuCategoryUpdate, MenuCategoryDelete, MenuItemCreate, MenuItemUpdate, \
+    MenuItemDelete
 
 urlpatterns = [
-    path('create-category/', MenuCategoryCreate.as_view())
+    path('createcategory/', MenuCategoryCreate.as_view()),
+    path('updatecategory/<int:category_id>/', MenuCategoryUpdate.as_view()),
+    path('deletecategory/<int:category_id>/', MenuCategoryDelete.as_view()),
+    path('createiteam/', MenuItemCreate.as_view()),
+    path('updateiteam/<int:menu_id>/', MenuItemUpdate.as_view()),
+    path('deleteiteam/<int:menu_id>/', MenuItemDelete.as_view()),
+
+
 ]
+
