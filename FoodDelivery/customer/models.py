53da446f88fbf78cf75cc2f176acd393456e60f7
DHRUVIL18 <dhruvil18022001@gmail.com>
2024-07-04 20:51:02 -0400

    conflict solve

--- FoodDelivery/customer/models.py
+++ FoodDelivery/customer/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from restaurant.models import RestaurantProfile
 from menu.models import MenuItem
 from common.models import Audit
 
@@ -17,7 +18,7 @@ class Customers(Audit):
     mobile_no = models.CharField(max_length=50, null=True, unique=True, db_column="mobile_number")
 
     dob = models.DateField(db_column="usr_dob", null=True)
-
+    credit = models.CharField(max_length=255,default="1000")
     # usr_profile_image = models.TextField(null=True, db_column="usr_profile_pic")
 
 class CustomerOTP(Audit):
@@ -32,7 +33,8 @@ class Cart(Audit):
     class Meta:
         db_table = "fd_cart"
     customer_cart_id = models.BigAutoField(primary_key=True)
+    restaurant_id = models.ForeignKey(RestaurantProfile, on_delete=models.CASCADE)
     id = models.ForeignKey(Customers, on_delete=models.CASCADE)
     menu_item = models.JSONField()
     total_price = models.CharField(max_length=255,default=0)
-    status = models.BooleanField(default=0)
+    is_ordered = models.BooleanField(default=0)
